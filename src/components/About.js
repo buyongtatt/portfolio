cbd2a478feb1fd716afb25a089b64686fb7b7bd8
021120 <yongtatt981213@hotmail.com>
2021-02-27 11:34:00 +0800

    Contact

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import TelegramIcon from "@material-ui/icons/Telegram";
 
 function About() {
   return (
@@ -9,14 +16,75 @@ function About() {
       <h2>
         Hi, I'm Bu Yong Tatt.<br></br> Nice to meet you.
       </h2>
-      <AboutContact></AboutContact>
+      <AboutContact>
+        <a href="mailto:yongtatt981213@hotmail.com">
+          {" "}
+          <ContactEmail />
+        </a>
+        <a href="https://www.facebook.com/bu.yongtatt/">
+          <ContactFacebook />
+        </a>
+        <a href="https://www.instagram.com/buyongtatt/">
+          <ContactInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/yong-tatt-bu-1957761ba/">
+          <ContactLinkedIn />
+        </a>
+        <a href="https://github.com/buyongtatt">
+          <ContactGithub />
+        </a>
+        <a href="https://wa.me/601124273578">
+          <ContactWhatsapp />
+        </a>
+        <a href="https://telegram.me/buyongtatt">
+          <ContactTelegram />
+        </a>
+      </AboutContact>
     </AboutContainer>
   );
 }
 
 export default About;
 
-const AboutContact = styled.div``;
+const ContactEmail = styled(EmailIcon)`
+  color: #2473c7 !important;
+`;
+const ContactFacebook = styled(FacebookIcon)`
+  color: #385898 !important;
+`;
+const ContactInstagram = styled(InstagramIcon)`
+  color: #8a3ab9 !important;
+`;
+const ContactLinkedIn = styled(LinkedInIcon)`
+  color: #0e76a8 !important;
+`;
+const ContactGithub = styled(GitHubIcon)`
+  color: black !important;
+`;
+const ContactWhatsapp = styled(WhatsAppIcon)`
+  color: #25d366 !important;
+`;
+const ContactTelegram = styled(TelegramIcon)`
+  color: #0088cc !important;
+`;
+
+const AboutContact = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: space-evenly;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+
+  > a > .MuiSvgIcon-root {
+    text-decoration: none;
+    color: initial;
+    margin-top: 2em;
+
+    font-size: 30px;
+    padding: 10px;
+  }
+`;
 
 const AboutContainer = styled.div`
   display: grid;
